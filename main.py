9e79ef09cf1126952c94a17330449f66a62be008
MrHexarobyte <120273837+MrHexarobyte@users.noreply.github.com>
2023-06-02 12:19:19 +0300

    main.py version 0.4
    - Added if function executable commands
    - Improved if function

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-count = 0
+
 # open file
 file1 = open('kakascript.txt', 'r')
 Lines = file1.readlines()
-#
+# find lines
+
+def handleCMD(cmd,at1='0',at2='0'):
+    if cmd == '#say':
+        return 'print("'+at1+'")'
+    if cmd == '#laka':
+        return 'print("'+(at1+at2)+'")'
+
+
 for line in Lines:
     code = line.strip()
     #   VARIABLE ASSIGNMENT
     if "is" in code and code.endswith('.'): 
-        count += 1
         n3 = code.replace('.','') 
         n4 = n3.replace('is','') 
         n5 = n4.split()
@@ -18,5 +25,9 @@ for line in Lines:
         n2 = n1.replace(')','') 
         n6 = n2.replace('is','') 
         n7= n6.split()
-        exec('if '+n7[0]+'=='+n7[1]+': print("THE ARGUMENT IS TRUE")')
+        # EXECUTING COMMANDS
+        if len(n7) == 5:
+            exec('if '+n7[0]+'=='+n7[1]+': '+handleCMD( n7[2] , n7[3] ,n7[4] ))
+        elif len(n7) == 4:
+            exec('if '+n7[0]+'=='+n7[1]+': '+handleCMD( n7[2] , n7[3] ))
         exec('if '+n7[0]+'!='+n7[1]+': print("THE ARGUMENT IS WRONG")')
